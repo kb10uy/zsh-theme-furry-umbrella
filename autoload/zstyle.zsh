4d6a037b866f6f4d30ef1e89683edec1806ba728
Yuu Kobayashi <kb10uy@kb10uy.org>
2018-10-06 07:55:50 +0900

    これいる?

---
+++ autoload/zstyle.zsh
@@ -0,0 +1,6 @@
+zstyle ':zle:*' word-chars " /=;@:{},|"
+zstyle ':zle:*' word-style unspecified
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+zstyle ':completion:*' ignore-parents parent pwd ..
+zstyle ':completion:*:sudo:*' command-path /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin /usr/X11R6/bin
+zstyle ':completion:*:processes' command 'ps x -o pid,s,args'
